0c6608be7ab0bc7526780210ea3295b23933e308
Richard Chien <richardchienthebest@gmail.com>
2020-05-08 16:15:14 +0800

    add STT and TTS before handling message and before sending message

--- milktea/__init__.py
+++ milktea/__init__.py
@@ -1,14 +1,37 @@
 from nonebot import *
 
+from .ai_vendor import tencent_ai
+
 
 @before_handle_message
 async def stt(bot: NoneBot, event: Event):
-    event.message.append(MessageSegment.text('\n\n----处理前'))
+    print('before_handle_message')
+    for seg in event.message:
+        if seg.type != 'record':
+            continue
+        speech_base64 = seg.data.get('base64')
+        if speech_base64:
+            text = await tencent_ai.stt(speech_base64)
+            if text:
+                seg.type = 'text'
+                seg.data = {'text': text}
+    event.message.reduce()
+    print('converted msg:', event.message)
 
 
 @before_send_message
 async def tts(bot: NoneBot, event: Event, message: Message):
-    message.append(MessageSegment.text('\n\n----发送前'))
+    print('before_send_message')
+    for seg in message:
+        if seg.type != 'text':
+            continue
+        text = seg.data.get('text')
+        if text:
+            speech_base64 = await tencent_ai.tts(text)
+            if speech_base64:
+                seg.type = 'record'
+                seg.data = {'base64': speech_base64}
+    message.reduce()
 
 
 @on_command('test')
@@ -19,6 +42,11 @@ async def test(session: CommandSession):
 @on_natural_language(keywords={'你好'})
 async def hello(session: NLPSession):
     return IntentCommand(100.0, 'echo', current_arg='你也好')
+
+
+@on_natural_language(keywords={'你是谁'})
+async def who(session: NLPSession):
+    return IntentCommand(100.0, 'echo', current_arg='我是奶茶！')
 
 
 def main(config):
