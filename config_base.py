017493641e9c0069c340ca6cb82559291ee311c9
Richard Chien <richardchienthebest@gmail.com>
2020-05-08 10:57:51 +0800

    adjust config

---
+++ config_base.py
@@ -0,0 +1,4 @@
+from nonebot.default_config import *
+
+TENCENT_AI_APP_ID = ''
+TENCENT_AI_API_KEY = ''
--- run.py
+++ run.py
@@ -1,5 +1,42 @@
+import os
+import sys
+
 import milktea
-import config
+
+deployment_env = os.getenv('DEPLOYMENT_ENV', 'dev').lower()
+
+config = None
+if deployment_env in ('dev', 'development'):
+    try:
+        import config_dev as config
+    except ImportError:
+        pass
+elif deployment_env in ('test', 'testing'):
+    try:
+        import config_test as config
+    except ImportError:
+        pass
+elif deployment_env in ('prod', 'production'):
+    try:
+        import config_prod as config
+    except ImportError:
+        pass
+
+if config is None:
+    try:
+        import config
+    except ImportError:
+        pass
+
+if config is None:
+    try:
+        import config_base as config
+    except ImportError:
+        pass
+
+if config is None:
+    print('There is no configuration file!', file=sys.stderr)
+    exit(1)
 
 if __name__ == '__main__':
     milktea.main(config)
