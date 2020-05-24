cd4c93c529ddc2e73c3a591c32eed29c917b3a6c
Richard Chien <richardchienthebest@gmail.com>
2020-05-24 20:33:41 +0800

    add config items

--- config_base.py
+++ config_base.py
@@ -2,5 +2,10 @@ from nonebot.default_config import *
 
 COMMAND_START = {'', '/', '!', '.'}
 
+# 腾讯 AI
 TENCENT_AI_APP_ID = ''
 TENCENT_AI_APP_KEY = ''
+
+# 聚合数据
+JUHE_JOKE_API_KEY = ''  # 笑话大全
+JUHE_IDIOM_API_KEY = ''  # 成语词典
